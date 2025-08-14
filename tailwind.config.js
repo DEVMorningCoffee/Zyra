5dcb5e73c85fd95005b0f39be1ef9a1b462d057e
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-08-13 22:28:36 -0400

    Added the image, text, and config them to be used across the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
     content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                white:{
+                    100: '#F0F0F0',
+                },
+                yellow: {
+                    100: '#F4C752',
+                    200: '#FFDE05'
+                },
+                gray:{
+                    100: '#645A47'
+                },
+                black: {
+                    100: '#1D1D1D'
+                },
+                error: '#FF343A',
+                success: '#FF343A',
+                info: '#216DFF'
+            },
+            fontFamily: {
+                "poppins": ["Poppins-Regular", "sans-serif"],
+                "poppins-medium": ["Poppins-Medium", "sans-serif"],
+                "poppins-bold": ["Poppins-Bold", "sans-serif"],
+                "poppins-extrabold": ["Poppins-ExtraBold", "sans-serif"],
+                "poppins-light": ["Poppins-Light", "sans-serif"],
+                "poppins-extralight": ["Poppins-ExtraLight", "sans-serif"],
+            }
+        },
     },
     plugins: [],
 }
